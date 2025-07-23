5eae56c7fd10670f22c84bcfb4a61715256bd0fc
sayanpal469 <sayanpal469@gmail.com>
2025-07-23 11:59:13 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <p className="text-5xl flex justify-center items-center h-screen">
+        Hello Aniket
+      </p>
+    </div>
+  );
+}
+
+export default App;
